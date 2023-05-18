d914924b4a3b0e79eacdf75e1695874479e81b9c
Isaac Pendergrass <isaac.pendergrass@gmail.com>
2023-05-17 18:25:37 -0700

    cleanup

--- modules/useCases/src/main/scala/controllers/BuildUsageReportController.scala
+++ modules/useCases/src/main/scala/controllers/BuildUsageReportController.scala
@@ -1,9 +1,9 @@
 package controllers
 
-import entities.Report
+import entities.{Report, UsageEvent}
 import gateways.{BillingAdjustments, CustomerAccounts, UsageArchive, UsageStore}
 
-import java.time._
+import java.time.*
 import java.util.NoSuchElementException
 import scala.util.{Failure, Success, Try}
 
@@ -31,20 +31,26 @@ case class BuildUsageReportController(
         for{
           usage <- usageStore.usage(start, end, acct.id)
           adjustments <- billingAdjustments.adjustments(start, end, acct.id)
-          compute = usage.filter(_.usageType == "compute").map(_.units).sum
-          storage = usage.filter(_.usageType == "storage").map(_.units).sum
-          bandwidth = usage.filter(_.usageType == "bandwidth").map(_.units).sum
+          units = computeUnits(usage)
           adjustment = adjustments.map(_.amount).sum
         } yield Report(acct.id,
           start,
           end,
           acct.currency,
           acct.tier,
-          calculate(compute, "compute"),
-          calculate(storage, "storage"),
-          calculate(bandwidth, "bandwidth"),
+          calculate(units._1, "compute"),
+          calculate(units._2, "storage"),
+          calculate(units._3, "bandwidth"),
           adjustment
         )
       case None => Failure(NoSuchElementException(s"Account $account does not exist"))
+  }
+
+  def computeUnits(usage: List[UsageEvent]): (Double, Double, Double) = {
+    (
+      usage.filter(_.usageType == "compute").map(_.units).sum,
+      usage.filter(_.usageType == "storage").map(_.units).sum,
+      usage.filter(_.usageType == "bandwidth").map(_.units).sum
+    )
   }
 }
